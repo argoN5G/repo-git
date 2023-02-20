c7e870f7279e2d9b1372e3f066840a8233a92742
argoN5G <etronbox@gmail.com>
2023-02-20 15:12:04 +0000

    git_commands

---
+++ calculator.py
@@ -0,0 +1,39 @@
+!#usr/bin/python3
+
+
+def add(x, y):
+    pass
+
+    
+def substruct(x, y):
+    pass
+
+
+def multiply(x, y):
+    pass
+
+
+def devide(x, y):
+    pass
+
+
+def square(x, y):
+    pass
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
