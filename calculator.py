fedc79eb52cc367a044077ff7fc042eaf5184d1a
argoN5G <etronbox@gmail.com>
2023-02-20 19:04:04 +0000

    aplltying cal operations

--- calculator.py
+++ calculator.py
@@ -1,29 +1,51 @@
+###
 #!/usr/bin/python3
 
+import sys
 
 def add(x, y):
     return x+y
 
     
 def substruct(x, y):
-    pass
+    return x-y
 
 
 def multiply(x, y):
-    pass
+    return x*y
 
 
 def devide(x, y):
-    pass
+    if str(x/y)[2] == 0:
+         return str(x/y)[2]
+    else:
+        return x/y
 
 
 def square(x, y):
-    pass
+    return x ** y
 
-print("!*!*!*   X & Y must be integer numbers :   ")
-X = int(input("print X: "))
-Y = int(input("Enter Y: "))
-print("addition: ", add(X,Y))
+
+
+print("!*!*!*   X & Y must be integers numbers :   ")
+# take user input & store it in variables
+X = int(input("Enter X= "))
+Y = int(input("Enter Y= "))
+
+
+# print results to the console
+print("\n****\t\tcommon operations are: \t\t****\n")
+print("1- Addition: \t\t", add(X,Y))
+print("2- Substraction: \t", substruct(X,Y))
+print("3- Multiplication: \t", multiply(X,Y))
+print("4- Devision: \t\t", devide(X,Y))
+print("4- Square: \t\t", square(X,Y))
+
+print(str(X/Y)[2])
+ 
+
+
+
 
 
 
